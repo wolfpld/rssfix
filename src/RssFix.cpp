c79ddb4c8cf4d487a6680862d326a7a4a936bfbb
Bartosz Taudul <wolf.pld@gmail.com>
2020-09-26 02:51:45 +0200

    Load configuration file.

--- src/RssFix.cpp
+++ src/RssFix.cpp
@@ -7,6 +7,8 @@
 #include "OpenSslThreading.hpp"
 #include "Version.hpp"
 
+#include "../contrib/ini/ini.h"
+
 void PrintHelp()
 {
     printf( "\nUsage:\n" );
@@ -18,7 +20,7 @@ int main( int argc, char** argv )
 {
     printf( BOLDYELLOW "RssFix %i.%i.%i" RESET "\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH );
 
-    const char* configFile = "config.ini";
+    const char* configFile = "../config.ini";
 
     int c;
     while( ( c = getopt( argc, argv, "hc:" ) ) != -1 )
@@ -36,14 +38,23 @@ int main( int argc, char** argv )
         }
     }
 
+    auto config = ini_load( configFile );
+    if( !config )
+    {
+        fprintf( stderr, BOLDRED "Unable to open config file %s!" RESET "\n", configFile );
+        return -3;
+    }
+
     OpenSslThreadInit();
     if( curl_global_init( CURL_GLOBAL_ALL ) != 0 )
     {
         fprintf( stderr, BOLDRED "Unable to initialize libcurl!" RESET "\n" );
+        ini_free( config );
         return -1;
     }
 
 
+    ini_free( config );
     curl_global_cleanup();
     OpenSslThreadCleanup();
     return 0;
