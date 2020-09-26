3193a1fb85611f506684cad216ffe8f98a22d52c
Bartosz Taudul <wolf.pld@gmail.com>
2020-09-26 02:41:56 +0200

    Parse command line parameters.

--- src/RssFix.cpp
+++ src/RssFix.cpp
@@ -1,14 +1,40 @@
 #include <curl/curl.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "Color.hpp"
 #include "OpenSslThreading.hpp"
 #include "Version.hpp"
 
+void PrintHelp()
+{
+    printf( "\nUsage:\n" );
+    printf( "   -h              this help message\n" );
+    printf( "   -c config.ini   set path to configuration file\n" );
+}
+
 int main( int argc, char** argv )
 {
     printf( BOLDYELLOW "RssFix %i.%i.%i" RESET "\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH );
+
+    const char* configFile = "config.ini";
+
+    int c;
+    while( ( c = getopt( argc, argv, "hc:" ) ) != -1 )
+    {
+        switch( c )
+        {
+        case 'h':
+            PrintHelp();
+            return 0;
+        case 'c':
+            configFile = optarg;
+            break;
+        default:
+            return -2;
+        }
+    }
 
     OpenSslThreadInit();
     if( curl_global_init( CURL_GLOBAL_ALL ) != 0 )
