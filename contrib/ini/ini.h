60e2f370b4b41ac0c5221f5e5a0f26a953f36121
Bartosz Taudul <wolf.pld@gmail.com>
2020-09-26 02:50:01 +0200

    Support C++ in contrib/ini.

--- contrib/ini/ini.h
+++ contrib/ini/ini.h
@@ -10,11 +10,19 @@
 
 #define INI_VERSION "0.1.1"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct ini_t ini_t;
 
 ini_t*      ini_load(const char *filename);
 void        ini_free(ini_t *ini);
 const char* ini_get(ini_t *ini, const char *section, const char *key);
 int         ini_sget(ini_t *ini, const char *section, const char *key, const char *scanfmt, void *dst);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
