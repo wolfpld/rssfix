52a4a1b8233f89086c8165022925bcd6e22c40e6
Bartosz Taudul <wolf.pld@gmail.com>
2020-10-04 02:14:22 +0200

    Rewrite job system.
    Simpler logic, proper behavior.

--- src/JobSystem.cpp
+++ src/JobSystem.cpp
@@ -11,6 +11,7 @@ JobSystem::JobSystem( int threads )
     assert( threads > 0 );
     printf( "Job system init with %i threads\n", threads );
     fflush( stdout );
+    m_controller = std::thread( [this] { Controller(); } );
     for( int i=0; i<threads; i++ ) m_threadPool.emplace_back( std::thread( [this] { Worker(); } ) );
 }
 
@@ -20,56 +21,85 @@ JobSystem::~JobSystem()
     fflush( stdout );
     m_exit.store( true, std::memory_order_release );
     {
-        std::lock_guard lock( m_jobLock );
-        m_jobCv.notify_all();
+        std::lock_guard lock( m_waitLock );
+        m_waitCv.notify_all();
+    }
+    {
+        std::lock_guard lock( m_runLock );
+        m_runCv.notify_all();
     }
     for( auto& v : m_threadPool ) v.join();
+    m_controller.join();
     printf( "Job system shutdown done\n" );
     fflush( stdout );
 }
 
 void JobSystem::Enqueue( int64_t runAt, void(*task)(Handler*), Handler* hnd )
 {
-    std::lock_guard lock( m_jobLock );
-    m_jobQueue.emplace_back( JobQueue { runAt, task, hnd } );
-    std::push_heap( m_jobQueue.begin(), m_jobQueue.end(), [] ( const auto& l, const auto& r ) { return l.runAt > r.runAt; } );
-    m_jobCv.notify_all();
+    const auto ct = std::chrono::duration_cast<std::chrono::seconds>( std::chrono::steady_clock::now().time_since_epoch() ).count();
+    if( runAt <= ct )
+    {
+        std::lock_guard lock( m_runLock );
+        m_runQueue.emplace_back( JobQueue { runAt, task, hnd } );
+        m_runCv.notify_one();
+    }
+    else
+    {
+        std::lock_guard lock( m_waitLock );
+        m_waitQueue.emplace_back( JobQueue { runAt, task, hnd } );
+        std::push_heap( m_waitQueue.begin(), m_waitQueue.end(), [] ( const auto& l, const auto& r ) { return l.runAt > r.runAt; } );
+        m_waitCv.notify_one();
+    }
+}
+
+void JobSystem::Controller()
+{
+    std::unique_lock lock( m_waitLock );
+    for(;;)
+    {
+        if( m_exit.load( std::memory_order_acquire ) ) return;
+        if( m_waitQueue.empty() )
+        {
+            m_waitCv.wait( lock );
+        }
+        else
+        {
+            const auto ct = std::chrono::duration_cast<std::chrono::seconds>( std::chrono::steady_clock::now().time_since_epoch() ).count();
+            const auto ft = m_waitQueue.front().runAt;
+            if( ct < ft )
+            {
+                m_waitCv.wait_for( lock, std::chrono::seconds( ft - ct ) );
+            }
+            else
+            {
+                std::pop_heap( m_waitQueue.begin(), m_waitQueue.end(), [] ( const auto& l, const auto& r ) { return l.runAt > r.runAt; } );
+                auto job = std::move( m_waitQueue.back() );
+                m_waitQueue.pop_back();
+                std::lock_guard runLock( m_runLock );
+                m_runQueue.emplace_back( std::move( job ) );
+                m_runCv.notify_one();
+            }
+        }
+    }
 }
 
 void JobSystem::Worker()
 {
-    std::unique_lock lock( m_jobLock );
+    std::unique_lock lock( m_runLock );
     for(;;)
     {
         if( m_exit.load( std::memory_order_acquire ) ) return;
-        bool shouldWait = true;
-        const auto ct = std::chrono::duration_cast<std::chrono::seconds>( std::chrono::steady_clock::now().time_since_epoch() ).count();
-        if( !m_jobQueue.empty() )
+        if( m_runQueue.empty() )
         {
-            if( m_jobQueue.front().runAt <= ct )
-            {
-                shouldWait = false;
-                auto job = std::move( m_jobQueue.front() );
-                pop_heap( m_jobQueue.begin(), m_jobQueue.end(), [] ( const auto& l, const auto& r ) { return l.runAt > r.runAt; } );
-                m_jobQueue.pop_back();
-                lock.unlock();
-                job.task( job.hnd );
-                lock.lock();
-                if( m_exit.load( std::memory_order_acquire ) ) return;
-            }
+            m_runCv.wait( lock );
         }
-        if( shouldWait )
+        else
         {
-            if( m_jobQueue.empty() )
-            {
-                m_jobCv.wait( lock );
-            }
-            else
-            {
-                const auto ft = m_jobQueue.front().runAt;
-                assert( ft > ct );
-                m_jobCv.wait_for( lock, std::chrono::seconds( ft - ct ) );
-            }
+            auto job = std::move( m_runQueue.back() );
+            m_runQueue.pop_back();
+            lock.unlock();
+            job.task( job.hnd );
+            lock.lock();
         }
     }
 }
--- src/JobSystem.hpp
+++ src/JobSystem.hpp
@@ -26,14 +26,20 @@ public:
     void Enqueue( int64_t runAt, void(*task)(Handler*), Handler* hnd );
 
 private:
+    void Controller();
     void Worker();
 
     std::atomic<bool> m_exit;
+    std::thread m_controller;
     std::vector<std::thread> m_threadPool;
 
-    std::mutex m_jobLock;
-    std::condition_variable m_jobCv;
-    std::vector<JobQueue> m_jobQueue;
+    std::mutex m_waitLock;
+    std::condition_variable m_waitCv;
+    std::vector<JobQueue> m_waitQueue;
+
+    std::mutex m_runLock;
+    std::condition_variable m_runCv;
+    std::vector<JobQueue> m_runQueue;
 };
 
 #endif
